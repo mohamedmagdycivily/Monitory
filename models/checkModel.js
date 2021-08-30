a731b00e096b4d381b9e7d1504810dcc64bc89c2
mohamed magdy <mohamedmagdycivily@gmail.com>
2021-08-30 19:07:20 +0200

    check model

---
+++ models/checkModel.js
@@ -0,0 +1,88 @@
+const mongoose = require("mongoose");
+// const validator = require('validator');
+const authenticationSchema = new mongoose.Schema({
+  username: { type: String },
+  password: { type: String },
+});
+
+const checkSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "A check must have a name"],
+      trim: true,
+    },
+    url: {
+      type: String,
+      required: [true, "A check must have a url"],
+    },
+    protocol: {
+      type: String,
+      required: [true, "A check must have a difficulty"],
+      enum: {
+        values: ["http", "https", "tcp"],
+        message: "protocol is either: http, https, tcp",
+      },
+    },
+    path: {
+      type: String,
+    },
+    port: {
+      type: Number,
+    },
+    webhook: {
+      type: String,
+    },
+    timeout_seconds: {
+      type: Number,
+      default: 5,
+    },
+    interval_minutes: {
+      type: Number,
+      default: 10,
+    },
+    threshold: {
+      type: Number,
+      default: 1,
+    },
+    authentication: {
+      username: { type: String },
+      password: { type: String },
+    },
+    httpHeaders: {
+      type: Map,
+      of: String,
+    },
+    assert: {
+      statusCode: { type: Number },
+    },
+    tags: [String],
+    ignoreSSL: {
+      type: Boolean,
+      default: true,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Check must belong to a user"],
+    },
+    pause: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+checkSchema.pre("save", function (next) {
+  next();
+});
+
+checkSchema.index({ name: 1, user: 1 }, { unique: true });
+
+const Check = mongoose.model("Check", checkSchema);
+
+module.exports = Check;
