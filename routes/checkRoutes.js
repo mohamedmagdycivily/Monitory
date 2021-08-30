861651fc41f78e2d9d03b451b88eed20bdd929cb
mohamed magdy <mohamedmagdycivily@gmail.com>
2021-08-30 19:08:13 +0200

    check routes

---
+++ routes/checkRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const authController = require("../controllers/authController");
+const checkController = require("../controllers/checkController");
+
+const router = express.Router();
+
+router.use(authController.protect);
+
+router.route("/").post(checkController.createCheck);
+router
+  .route("/:id")
+  .patch(checkController.updateCheck)
+  .delete(checkController.deleteCheck);
+
+module.exports = router;
