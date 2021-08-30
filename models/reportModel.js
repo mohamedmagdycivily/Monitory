86791784c3080cc2921bf9daf21a90df2b4c7f09
mohamed magdy <mohamedmagdycivily@gmail.com>
2021-08-30 19:07:49 +0200

    report model

---
+++ models/reportModel.js
@@ -0,0 +1,42 @@
+const { CheckStatus } = require("../utils/constants");
+const mongoose = require("mongoose");
+// const validator = require('validator');
+
+const reportSchema = new mongoose.Schema(
+  {
+    status: {
+      type: String,
+      enum: {
+        values: Object.values(CheckStatus),
+        message: "status is either: up or down",
+      },
+    },
+    availability: {
+      type: Number,
+    },
+    outages: {
+      type: Number,
+    },
+    downtime: {
+      type: Number,
+    },
+    uptime: {
+      type: Number,
+    },
+    responseTime: {
+      type: Number,
+    },
+    check: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Check",
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Report = mongoose.model("Report", reportSchema);
+
+module.exports = Report;
