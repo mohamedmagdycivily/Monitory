b824f011dd07524cb5afe26e28800b9367d98708
mohamed magdy <mohamedmagdycivily@gmail.com>
2021-08-30 19:08:38 +0200

    axios config

---
+++ utils/axios.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+
+axios.interceptors.request.use(
+  function (config) {
+    config.metadata = { startTime: new Date() };
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+axios.interceptors.response.use(
+  function (response) {
+    response.config.metadata.endTime = new Date();
+    response.duration =
+      response.config.metadata.endTime - response.config.metadata.startTime;
+    response.duration = Math.round(response.duration / 1000);
+    return response;
+  },
+  function (error) {
+    error.config.metadata.endTime = new Date();
+    error.duration =
+      error.config.metadata.endTime - error.config.metadata.startTime;
+    error.duration = Math.round(error.duration / 1000);
+    return Promise.reject(error);
+  }
+);
+
+module.exports = axios;
